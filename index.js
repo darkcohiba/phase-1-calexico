d3a3d34304c231f70f317a6f2423457a3bd8283d
Sam <samuel.waters@gmail.com>
2022-12-22 13:35:56 -0700

    finished walkthrough

--- index.js
+++ index.js
@@ -1,48 +1,68 @@
+//track the current food item in the global scale
+let currentItem;
 
-
-//fetch request
+//fetch request to get data from server
 fetch("http://localhost:3000/menu")
-.then(response => response.json())
-.then(menuData => {
-    buildMenu(menuData);
-    setDish(menuData[0])
-    setupCart();
-});
+    .then(response => response.json())
+    .then(data => {
+        //wishlist of functions
+        //render our titles to the menu as a span
+        renderMenu(data);
+        //show the details and image
+        showDetails(data[0]);
+        //function to add numbers to the cart
+        addToCart();
+    })
 
 
+function renderMenu(data) {
+    let menu = document.querySelector('#menu-items')
+    data.forEach(item => {
+        let menuItem = document.createElement('span')
+        menuItem.textContent = item.name
+        menu.append(menuItem)
 
-//building out menu on the side
-function buildMenu(menuItems) {
-    let menuList = document.querySelector("#menu-items");
-    menuItems.forEach(item => {
-        let menuListItem = document.createElement("span");
-        menuListItem.textContent = item.name;
-        menuList.appendChild(menuListItem);
-
-        menuListItem.addEventListener('click', () => {
-            setDish(item)
+        //challenge 3: add event listener to run showdetails
+        menuItem.addEventListener('click', () =>{
+            showDetails(item);
         })
+    })
+}
+
+//challenge 2: show details
+function showDetails(data) {
+    currentItem = data;
+
+    let image = document.querySelector('#dish-image')
+    let name = document.querySelector('#dish-name')
+    let price = document.querySelector('#dish-price')
+    let description = document.querySelector("#dish-description")
+    let bagNumber = document.querySelector('#number-in-cart')
+
+    bagNumber.textContent =data.number_in_bag
+    image.src = data.image;
+    name.textContent = data.name;
+    price.textContent = data.price
+    description.textContent = data.description
+}
+
+function addToCart(){
+    //query selector for the cart form
+    let cartForm = document.querySelector('#cart-form')
+    //event listener for the cart form
+
+    cartForm.addEventListener('submit', (e) =>{
+        //prevent default
+        e.preventDefault();
+        //track the current objects number in the bag and ad the current number to it
+        currentItem.number_in_bag += parseInt(e.target['cart-amount'].value)
+        showDetails(currentItem);
     });
+
 }
 
 
-function setDish(dish) {
 
-    //updating global varaible to the current dish that is set
-    currentDish = dish;
 
-    //variables for dish
-    let dishImage = document.querySelector("#dish-image");
-    let dishName = document.querySelector("#dish-name");
-    let dishDescription = document.querySelector("#dish-description");
-    let dishPrice = document.querySelector("#dish-price");
-    let numberInCart = document.querySelector("#number-in-cart");
-    
 
-    //updating text to be the current dish
-    dishImage.src = dish.image;
-    dishName.textContent = dish.name;
-    dishDescription.textContent = dish.description;
-    dishPrice.textContent = `$${dish.price}`;
-    numberInCart.textContent = dish.number_in_bag;
-}
+
--- solution.js
+++ solution.js
@@ -1,5 +1,6 @@
+//set global variable
 let currentDish;
-
+//fetch request
 fetch("http://localhost:3000/menu")
 .then(response => response.json())
 .then(menuData => {
@@ -22,21 +23,25 @@ function buildMenu(menuData) {
 }
 
 function setDish(dish) {
+        //updating global varaible to the current dish that is set
+
     currentDish = dish;
+    //variables for dish
 
     let dishImage = document.querySelector("#dish-image");
     let dishName = document.querySelector("#dish-name");
     let dishDescription = document.querySelector("#dish-description");
     let dishPrice = document.querySelector("#dish-price");
     let numberInCart = document.querySelector("#number-in-cart");
-    
+        //updating text to be the current dish
+
     dishImage.src = dish.image;
     dishName.textContent = dish.name;
     dishDescription.textContent = dish.description;
     dishPrice.textContent = `$${dish.price}`;
     numberInCart.textContent = dish.number_in_bag;
 }
-
+//add the cart functionality
 function setupCart() {
     let addToCartForm = document.querySelector("#cart-form")
     addToCartForm.addEventListener('submit', (event) => {
@@ -46,3 +51,54 @@ function setupCart() {
         addToCartForm.reset();
     });
 }
+
+
+
+
+fetch("http://localhost:3000/menu")
+.then(response => response.json())
+.then(menuData => {
+    buildMenu(menuData);
+    setDish(menuData[0])
+    setupCart();
+});
+
+
+
+//building out menu on the side
+function buildMenu(menuItems) {
+    let menuList = document.querySelector("#menu-items");
+    menuItems.forEach(item => {
+        let menuListItem = document.createElement("span");
+        menuListItem.textContent = item.name;
+        menuList.appendChild(menuListItem);
+
+        menuListItem.addEventListener('click', () => {
+            setDish(item)
+        })
+    });
+}
+
+
+function setDish(dish) {
+
+    //updating global varaible to the current dish that is set
+    currentDish = dish;
+
+    //variables for dish
+    let dishImage = document.querySelector("#dish-image");
+    let dishName = document.querySelector("#dish-name");
+    let dishDescription = document.querySelector("#dish-description");
+    let dishPrice = document.querySelector("#dish-price");
+    let numberInCart = document.querySelector("#number-in-cart");
+    
+
+    //updating text to be the current dish
+    dishImage.src = dish.image;
+    dishName.textContent = dish.name;
+    dishDescription.textContent = dish.description;
+    dishPrice.textContent = `$${dish.price}`;
+    numberInCart.textContent = dish.number_in_bag;
+}
+
+//add the cart functionality

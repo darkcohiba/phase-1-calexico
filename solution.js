f369907c80dad71d4e18d1b20b7a39cde6ec256c
Sam <samuel.waters@gmail.com>
2022-12-22 12:38:27 -0700

    finished

--- solution.js
+++ solution.js
@@ -1,6 +1,5 @@
+let currentDish;
 
-
-//fetch request
 fetch("http://localhost:3000/menu")
 .then(response => response.json())
 .then(menuData => {
@@ -9,12 +8,9 @@ fetch("http://localhost:3000/menu")
     setupCart();
 });
 
-
-
-//building out menu on the side
-function buildMenu(menuItems) {
+function buildMenu(menuData) {
     let menuList = document.querySelector("#menu-items");
-    menuItems.forEach(item => {
+    menuData.forEach(item => {
         let menuListItem = document.createElement("span");
         menuListItem.textContent = item.name;
         menuList.appendChild(menuListItem);
@@ -25,24 +21,28 @@ function buildMenu(menuItems) {
     });
 }
 
-
 function setDish(dish) {
-
-    //updating global varaible to the current dish that is set
     currentDish = dish;
 
-    //variables for dish
     let dishImage = document.querySelector("#dish-image");
     let dishName = document.querySelector("#dish-name");
     let dishDescription = document.querySelector("#dish-description");
     let dishPrice = document.querySelector("#dish-price");
     let numberInCart = document.querySelector("#number-in-cart");
     
-
-    //updating text to be the current dish
     dishImage.src = dish.image;
     dishName.textContent = dish.name;
     dishDescription.textContent = dish.description;
     dishPrice.textContent = `$${dish.price}`;
     numberInCart.textContent = dish.number_in_bag;
+}
+
+function setupCart() {
+    let addToCartForm = document.querySelector("#cart-form")
+    addToCartForm.addEventListener('submit', (event) => {
+        event.preventDefault();
+        currentDish.number_in_bag += parseInt(event.target["cart-amount"].value);
+        setDish(currentDish);
+        addToCartForm.reset();
+    });
 }

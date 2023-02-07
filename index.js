535f44417898fb412f081a5e50e7c5d239e237c6
Sam <samuel.waters@gmail.com>
2023-02-07 16:02:19 -0700

    more

--- index.js
+++ index.js
@@ -1,68 +1,58 @@
-//track the current food item in the global scale
-let currentItem;
+//global variable for current meal option
+let currentDish;
 
-//fetch request to get data from server
 fetch("http://localhost:3000/menu")
     .then(response => response.json())
-    .then(data => {
+    .then(menuData =>{
         //wishlist of functions
-        //render our titles to the menu as a span
-        renderMenu(data);
-        //show the details and image
-        showDetails(data[0]);
-        //function to add numbers to the cart
-        addToCart();
+        //render menu items to menu-items div
+        renderMenu(menuData)
+        //render item details, should start with first item
+        renderDetails(menuData[0])
+        //function to add number of items to the cart
+        addToCart()
     })
 
-
-function renderMenu(data) {
+function renderMenu(menuData){
     let menu = document.querySelector('#menu-items')
-    data.forEach(item => {
-        let menuItem = document.createElement('span')
+    menuData.forEach(item => {
+        let menuItem = document.createElement("span")
         menuItem.textContent = item.name
         menu.append(menuItem)
 
-        //challenge 3: add event listener to run showdetails
-        menuItem.addEventListener('click', () =>{
-            showDetails(item);
+        //challenge #3
+        menuItem.addEventListener("click", ()=>{
+            renderDetails(item)
         })
     })
 }
 
-//challenge 2: show details
-function showDetails(data) {
-    currentItem = data;
-
-    let image = document.querySelector('#dish-image')
-    let name = document.querySelector('#dish-name')
-    let price = document.querySelector('#dish-price')
+function renderDetails(menuItem) {
+    currentDish = menuItem;
+    //selectors for details
+    let image = document.querySelector("#dish-image")
+    let name = document.querySelector("#dish-name")
+    let price = document.querySelector("#dish-price")
     let description = document.querySelector("#dish-description")
-    let bagNumber = document.querySelector('#number-in-cart')
+    let numberInBag = document.querySelector("#number-in-cart")
 
-    bagNumber.textContent =data.number_in_bag
-    image.src = data.image;
-    name.textContent = data.name;
-    price.textContent = data.price
-    description.textContent = data.description
+    image.src = currentDish.image
+    numberInBag.textContent = currentDish.number_in_bag
+    price.textContent = currentDish.price
+    description.textContent = currentDish.description
+    name.textContent = currentDish.name
 }
+
 
 function addToCart(){
-    //query selector for the cart form
-    let cartForm = document.querySelector('#cart-form')
-    //event listener for the cart form
+    //query selector for cart form
+    let cartForm = document.querySelector("#cart-form")
 
-    cartForm.addEventListener('submit', (e) =>{
-        //prevent default
+    cartForm.addEventListener("submit", (e) => {
         e.preventDefault();
-        //track the current objects number in the bag and ad the current number to it
-        currentItem.number_in_bag += parseInt(e.target['cart-amount'].value)
-        showDetails(currentItem);
-    });
-
+        // console.log(currentDish.number_in_bag)
+        console.log(e)
+        currentDish.number_in_bag += parseInt(e.target['cart-amount'].value)
+        renderDetails(currentDish)
+    })
 }
-
-
-
-
-
-
